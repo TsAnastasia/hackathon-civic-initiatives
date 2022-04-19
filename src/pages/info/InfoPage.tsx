577139bdeae0a0562c67a08fdea62424ecb8a0c3
Anastasia Tsibernaya <anastasiacibernaja@yandex.ru>
2022-04-19 23:59:14 +0300

    style info page

--- src/pages/info/InfoPage.tsx
+++ src/pages/info/InfoPage.tsx
@@ -1,13 +1,14 @@
 import { legalDocs, serviceDocs } from "../../utils/docs";
 import InfoDocs from "./docs/InfoDocs";
+import styles from "./infoPage.module.scss";
 
 const InfoPage = () => {
   return (
-    <>
-      <h1>Info page</h1>
+    <div className={styles.root}>
+      <h1 className={styles.title}>Информация</h1>
       <InfoDocs title="Использование сервиса" docs={serviceDocs} />
       <InfoDocs title="Правовая информация" docs={legalDocs} />
-    </>
+    </div>
   );
 };
 
--- src/pages/info/docs/InfoDocs.tsx
+++ src/pages/info/docs/InfoDocs.tsx
@@ -2,15 +2,18 @@ import { FC } from "react";
 import { Link } from "react-router-dom";
 import { Docs } from "../../../types/doc";
 import { goToPage } from "../../../utils/routes";
+import styles from "./infoDocs.module.scss";
 
 const InfoDocs: FC<{ title: string; docs: Docs }> = ({ title, docs }) => {
   return (
-    <section>
-      <h2>{title}</h2>
-      <ul>
+    <section className={styles.root}>
+      <h2 className={styles.title}>{title}</h2>
+      <ul className={styles.list}>
         {Object.entries(docs).map(([id, { title }]) => (
-          <li key={id}>
-            <Link to={goToPage.doc(id)}>{title}</Link>
+          <li key={id} className={styles.item}>
+            <Link to={goToPage.doc(id)} className={styles.link}>
+              {title}
+            </Link>
           </li>
         ))}
       </ul>
