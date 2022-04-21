f946d6d572b84dccfa21728f9fa24e8cf35988db
Anastasia Tsibernaya <anastasiacibernaja@yandex.ru>
2022-04-21 11:40:53 +0300

    create breadcrumbs

--- src/pages/doc/DocPage.tsx
+++ src/pages/doc/DocPage.tsx
@@ -1,16 +1,26 @@
 import { useParams } from "react-router-dom";
+import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 import { docs } from "../../utils/docs";
+import { goToPage } from "../../utils/routes";
 import DocContent from "./content/DocContent";
 
 const DocPage = () => {
   const { docId } = useParams();
 
-  return docId && docs[docId] ? (
-    <DocContent doc={docs[docId]} />
-  ) : (
-    <div>
-      <p>Book not found</p>
-    </div>
+  return (
+    <>
+      <Breadcrumbs
+        parents={[{ title: "Информация", link: goToPage.info }]}
+        current={`${docId && docs[docId].title}`}
+      />
+      {docId && docs[docId] ? (
+        <DocContent doc={docs[docId]} />
+      ) : (
+        <div>
+          <p>Book not found</p>
+        </div>
+      )}
+    </>
   );
 };
 
