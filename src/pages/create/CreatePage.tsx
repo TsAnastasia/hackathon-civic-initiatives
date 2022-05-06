e602a9585cb51217fa0fe42b829dcf9b392c4dc8
TsAnastasia <anastasiacibernaja@yandex.ru>
2022-05-06 14:12:28 +0300

    make create page intro

--- src/pages/create/CreatePage.tsx
+++ src/pages/create/CreatePage.tsx
@@ -1,5 +1,25 @@
+import { useSearchParams } from "react-router-dom";
+import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
+import { goToPage } from "../../router/routes";
+import CreateCategory from "./intro/CreateCategory";
+
 const CreatePage = () => {
-  return <h1>create page</h1>;
+  const [params] = useSearchParams();
+  const category = params.get("category");
+
+  return category ? (
+    <section>
+      <Breadcrumbs
+        parents={[{ title: "Выбор категории", link: goToPage.create }]}
+        current={"Ввод данных"}
+      />
+      <form>
+        <input type="text" name="test" />
+      </form>
+    </section>
+  ) : (
+    <CreateCategory />
+  );
 };
 
 export default CreatePage;
---
+++ src/pages/create/intro/CreateCategory.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { categoriesAPI } from "../../../API-data/categories/categoriesAPI";
+import AppLoader from "../../../components/AppLoader/AppLoader";
+import Breadcrumbs from "../../../components/Breadcrumbs/Breadcrumbs";
+import { Category } from "../../../types/categories";
+import { TIMEOUT_API } from "../../../utils/constants";
+import CreateCategoryCard from "./card/CreateCategoryCard";
+import scss from "./createCategory.module.scss";
+
+const CreateCategory = () => {
+  const [categories, setCategories] = useState<Category[] | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCategories(categoriesAPI.getCategories());
+    }, TIMEOUT_API);
+  }, []);
+
+  return (
+    <>
+      <Breadcrumbs parents={[]} current={"Выбор категории"} />
+      <section className={scss.root}>
+        <h1 className={scss.title}>Выберите категорию</h1>
+        {categories ? (
+          <ol className={scss.list}>
+            {categories.map((category) => (
+              <li key={category.id}>
+                <CreateCategoryCard category={category} />
+              </li>
+            ))}
+          </ol>
+        ) : (
+          <AppLoader />
+        )}
+      </section>
+    </>
+  );
+};
+
+export default CreateCategory;
